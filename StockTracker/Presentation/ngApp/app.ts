69f71242101624ab760c1ce38071246fb97aae9b
Sarah Kim <sarahywloo@gmail.com>
2016-01-27 17:23:54 -0600

    A simple app to track personal stock portfolio: Search, login and register page

---
+++ StockTracker/Presentation/ngApp/app.ts
@@ -0,0 +1,56 @@
+﻿namespace StockTracker {
+    
+    angular.module('StockTracker', ['ngRoute']);
+
+    angular.module('StockTracker').factory('authInterceptor',
+        ($q: ng.IQService, $window: ng.IWindowService, $location: ng.ILocationService) => {
+            return {
+                request: (config) => {
+                    config.headers = config.headers || {};
+                    let token = $window.localStorage.getItem('token');
+                    if (token) {
+                        config.headers.Authorization = `Bearer ${token}`;
+                    }
+                    return config;
+                },
+                responseError: (response) => {
+                    if (response.status === 401) {
+                        $location.path('/login');
+                    }
+                    return $q.reject(response);
+                }
+            };
+        });
+
+    angular.module('StockTracker')
+        .config(function ($routeProvider: ng.route.IRouteProvider, $httpProvider: ng.IHttpProvider) {
+
+            $httpProvider.interceptors.push('authInterceptor');
+
+            $routeProvider.when('/', {
+                template: 'Stock Tracker'
+            });
+
+            $routeProvider.when('/login', {
+                templateUrl: '/Presentation/ngApp/views/login.html',
+                controller: StockTracker.Controllers.AuthController,
+                controllerAs: 'controller'
+            });
+
+            $routeProvider.when('/register', {
+                templateUrl: '/Presentation/ngApp/views/register.html',
+                controller: StockTracker.Controllers.AuthController,
+                controllerAs: 'controller'
+            });
+
+            $routeProvider.when('/profile', {
+                templateUrl: '/Presentation/ngApp/views/profile.html',
+                controller: StockTracker.Controllers.AuthController,
+                controllerAs: 'controller'
+            });
+
+            $routeProvider.when('/research', {
+                templateUrl: '/Presentation/ngApp/views/research.html'
+            });
+        });
+}
